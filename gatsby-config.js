7fecaec8b3c0bf002183e42c486df840839a632f
saxena uditanshu <uditanshusaxena@gmail.com>
2020-08-15 16:39:29 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `SolvProb`,
+        short_name: `SolvProb`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
