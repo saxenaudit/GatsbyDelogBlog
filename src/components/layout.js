02e474691a5251fc69cb64db69ad98c1ff432659
saxena uditanshu <uditanshusaxena@gmail.com>
2020-08-27 17:01:56 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>{new Date().getFullYear()} SolvProb &bull;<a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>{new Date().getFullYear()} SolvProb </p>
       </footer>
     </div>
   )
